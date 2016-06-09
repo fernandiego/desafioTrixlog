c8e6d71a3d301644c4643547565d760df3744ebf
Fox <fernandodiego.sn@gmail.com>
2016-06-08 23:56:14 -0300

    Tutorial completado com sucesso =D

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 //depois trazer pra cá a parte JS
+//Fazendo tutorial básico do leaflet
+//Exemplo do plunker
+var map = L.map('map', {
+  'center': [0, 0],
+  'zoom': 2
+});
+var marker = L.marker([51.5, -0.09]).addTo(map);
+
+
+var circle = L.circle([51.508, -0.11], 500, {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5
+}).addTo(map);
+
+var polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(map);
+
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
+
+var popup = L.popup()
+    .setLatLng([51.5, -0.09])
+    .setContent("I am a standalone popup.")
+    .openOn(map);
+
+    function onMapClick(e) {
+        alert("You clicked the map at " + e.latlng);
+    }
+
+    map.on('click', onMapClick);
+
+var tileLayer = L.tileLayer('https://{s}.tiles.mapbox.com/v4/{mapId}/{z}/{x}/{y}.png?access_token={token}', {
+  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+  subdomains: ['a','b','c','d'],
+  mapId: 'rakshak.l937n12c',
+  token: 'pk.eyJ1IjoicmFrc2hhayIsImEiOiJ5cHhqeHlRIn0.Vi87VjI1cKbl1lhOn95Lpw'
+});
+
+tileLayer.addTo(map);
