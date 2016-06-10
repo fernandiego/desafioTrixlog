1a99f13cfc03b40227d9c4aec53bac972ee5138d
Fox <fernandodiego.sn@gmail.com>
2016-06-10 04:09:47 -0300

    Pequena pausa

---
+++ script.js
@@ -0,0 +1,17 @@
+var app = angular
+          .module("myApp",  []);
+          app.directive("primeira", function(){
+            return {
+              template : "<h1>Made by a directive!</h1>"
+            };
+          });
+          app.directive("meuTeste", function(){
+            return {
+              template : "<h3>Testando</h3>"
+            };
+          });
+          app.directive("meuMapa", function(){
+            return {
+              template : "<h3>Testando</h3>"
+            };
+          });
