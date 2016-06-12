7c5efad3623063aa7c7d61ae7108b37dea545aaf
Fox <fernandodiego.sn@gmail.com>
2016-06-11 23:43:59 -0300

    Criação do mapa foi encapsulada com sucesso.

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
 var app = angular
           .module("myApp",  []);
-          app.directive("primeira", function(){
+          app.directive('myMap', [ function() {
             return {
-              template : "<h1>Made by a directive!</h1>"
+              replace: true,
+              template: '<div></div>',
+              link: function (scope, element, attributes) {
+                var map = L.map('map', {
+                  'center': [-3.73631, -38.54373],
+                  'zoom': 14
+                });
+                var tileLayer = L.tileLayer('https://{s}.tiles.mapbox.com/v4/{mapId}/{z}/{x}/{y}.png?access_token={token}', {
+                  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+                  subdomains: ['a','b','c','d'],
+                  mapId: 'rakshak.l937n12c',
+                  token: 'pk.eyJ1IjoicmFrc2hhayIsImEiOiJ5cHhqeHlRIn0.Vi87VjI1cKbl1lhOn95Lpw'
+                });
+
+                tileLayer.addTo(map);
+
+              }
             };
-          });
-          app.directive("meuTeste", function(){
-            return {
-              template : "<h3>Testando</h3>"
+          }
+        ]);
+        app.directive('myMarker', [ function() {
+          return {
+              replace: true,
+              template: '<div></div>',
+              link: function (scope, element, attributes) {
+                
+              }
             };
-          });
-          app.directive("meuMapa", function(){
-            return {
-              template : "<h3>Testando</h3>"
-            };
-          })
-          app.directive('myCustomer', function() {
-            return {
-              templateUrl: 'index-maps.html'
-            };
-          });
+          }
+        ]);
