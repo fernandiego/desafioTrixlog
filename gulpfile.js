7075a35a9a27a8a740356344fb397a06608737c0
Fox <fernandodiego.sn@gmail.com>
2016-06-14 02:02:56 -0300

    Pequena alteração no css

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 
 gulp.task('jshint', function()  {
-  gulp.src('js/*.js')
+  return gulp.src('*.js')
   .pipe(jshint())
   .pipe(jshintreporter('default'));
 });
