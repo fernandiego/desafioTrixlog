85c803bc1511e0ebfd7f029ffcce48eb7e88b448
Fox <fernandodiego.sn@gmail.com>
2016-06-14 18:04:25 -0300

    pequena alteração no gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
-var concat = require('gulp-concat');
 
 gulp.task('jshint', function()  {
   return gulp.src('script.js')
